62cb10a94ae97e343ff56cd8e3c0e8d9c3e4706e
ildar <ildar@localhost.localdomain>
2016-04-22 22:16:16 +0300

    some ideas

---
+++ ConvexHullTrickOnline/src/CHTONLINE.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ildar on 22.04.16.
+ */
+//WILL BE SOON!
+public class CHTOFFLINE {
+    public class Line {
+        long a, b;
+    }
+    TreeSet <Line> s = new TreeSet <>();
+}
